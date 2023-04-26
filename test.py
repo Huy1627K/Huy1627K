0c17bddeecd27d89f7c6e2c593f8956c64fa1843
Quang Huy <huyhuyhuyhuy1611@gmail.com>
2023-04-26 16:00:43 +0700

    1

---
+++ test.py
@@ -0,0 +1,15 @@
+# def emailProcess(email):
+#     email_username = email[0:email.index("@")]
+#     email_domain = email[email.index("@") + 1:]
+#     return [email_username, email_domain]
+
+# def printMsg(email_username, email_domain):
+#     print(f"username is {email_username};email_domain is {email_domain}")
+
+# def main():
+#     email = input("Please enter your email address: ")
+#     email_username, email_domain = emailProcess(email)
+#     printMsg(email_username, email_domain)
+
+# if __name__ == "__main__":
+#     main()
